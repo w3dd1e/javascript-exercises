603562cdad90ce126a16f369564f64c7ed98ea46
w3dd1e <kelly.weddle@gmail.com>
2025-01-24 23:26:47 -0600

    Exercise passed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let aZ09 = "abcdefghihklmnopqrstuvwxyz0123456789"
+    let onlyString =
+        string.toLowerCase().split('').filter(character => aZ09.includes(character)).join('');
+    return onlyString === onlyString.split('').reverse().join('');
 };
 
 // Do not edit below this line
