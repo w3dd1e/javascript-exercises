e5c24fe0f04434f985c8e99c614cd97670c093a4
w3dd1e <kelly.weddle@gmail.com>
2025-01-24 22:44:19 -0600

    Exercise passed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fTemp) {
+  let cTemp = (fTemp-32)* (5/9)
+  return +cTemp.toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cTemp) {
+  let fTemp = cTemp * (9/5) + 32
+  return +fTemp.toFixed(1);
 };
 
 // Do not edit below this line
