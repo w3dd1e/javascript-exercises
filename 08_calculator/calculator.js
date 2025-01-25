c0740bad9f530f37e323a38cf4f0d4af758932a5
w3dd1e <kelly.weddle@gmail.com>
2025-01-24 23:07:27 -0600

    Exercise passed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let sum = 0;
+    for (let num of array){
+      sum += num;
+    }
+    return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let total = 1;
+  for(let num of array) {
+    total *= num;
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let total = 1;
+  while(num>0){
+    total *= num;
+    num--;
+  }
+  return total;
 };
 
 // Do not edit below this line
