18176b51ef9488de49a54c76e673c0b19e3d6e03
w3dd1e <kelly.weddle@gmail.com>
2025-01-25 00:39:05 -0600

    Exercise passed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+    for (let item of people) {
+        if (item.yearOfDeath === undefined) {
+            item.yearOfDeath = 2025
+        }
+    }
+    let compareYears = (a, b) => {
+        if (Math.abs(a.yearOfDeath - a.yearOfBirth) > Math.abs(b.yearOfDeath - b.yearOfBirth)) return 1;
+        if (Math.abs(a.yearOfDeath - a.yearOfBirth) === Math.abs(b.yearOfDeath - b.yearOfBirth)) return 0;
+        if (Math.abs(a.yearOfDeath - a.yearOfBirth) < Math.abs(b.yearOfDeath - b.yearOfBirth)) return -1;
+    }
 
+    people.sort(compareYears);
+    return people[people.length - 1];
 };
-
 // Do not edit below this line
 module.exports = findTheOldest;
