48d43bba55d64c8c5d372f19c6f17655b154c8f0
w3dd1e <kelly.weddle@gmail.com>
2025-01-24 23:43:36 -0600

    Exercise passed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,23 @@
-const fibonacci = function() {
+
+//F[n-1] + F[n-2] while n>1
+const fibonacci = function(number) {
+    let num = +number;
+    let fArr = [0,1];
+    if (num < 0) {
+        return "OOPS"
+    }
+    else {
+        for (let i = 2; i <= num; i++) {
+            fArr.push(fArr[i-1]+fArr[i-2]);
+
+        }
+        return fArr[num];
+
+    }
 
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+
+
