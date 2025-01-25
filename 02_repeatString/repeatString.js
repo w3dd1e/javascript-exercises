945b28f9677a4b1f320b84567b5144e3f05eb9e4
w3dd1e <kelly.weddle@gmail.com>
2025-01-24 20:28:43 -0600

    Exercise completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let response ='';
+    if (num<0){
+        return "ERROR";
+    }
+    for (let i = 0; i < num; i++) {
+        response += string;
+    }
+    return response;
+}
 
-};
 
 // Do not edit below this line
 module.exports = repeatString;
